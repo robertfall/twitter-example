0134f32d4839054f42daa82e064eaad0d2d50b5c
Robert Fall <rob@robertfall.com>
2015-08-26 09:47:41 +0200

    Add factory method to FollowerMap

--- follower_map.rb
+++ follower_map.rb
@@ -5,7 +5,9 @@ require 'minitest/autorun'
 
 class FollowerMap
   def self.from_list(follows)
-
+    new.tap { |map|
+      follows.each { |follow| map.add_follow(follow) }
+    }
   end
 
   def initialize
@@ -28,6 +30,18 @@ class FollowerMap
 end
 
 describe FollowerMap do
+  describe '.from_list' do
+    let (:follows) {[Follow.new('Rob', 'Zane'), Follow.new('Zane', 'Paul')]}
+
+    it 'adds a follow for each item in the list' do
+      map = FollowerMap.from_list(follows)
+
+      map.broadcasters.count.must_equal 2
+      map.followers_for('Rob').must_include 'Zane'
+      map.followers_for('Zane').must_include 'Paul'
+    end
+  end
+
   describe '#followers_for' do
     let (:map) { FollowerMap.new }
 
