5c54043baba0649389378ba71957671ddf281c25
Robert Fall <rob@robertfall.com>
2015-08-26 09:18:41 +0200

    Invert UserLine to produce follows instead of followers

--- user_line.rb
+++ user_line.rb
@@ -1,4 +1,8 @@
-require "minitest/autorun"
+require 'set'
+
+require 'minitest/autorun'
+
+Follow = Struct.new(:broadcaster, :subscriber)
 
 class UserLine
   def initialize(line)
@@ -9,12 +13,14 @@ class UserLine
     VALID_PATTERN === @line
   end
 
-  def follows_by_user
+  def follows
     return {} unless valid?
 
     # TODO: Handle case issues
-    left, right = @line.split('follows').map(&:strip)
-    { left => right.split(',').map(&:strip) }
+    subscriber, broadcasters = @line.split('follows').map(&:strip)
+    broadcasters.split(',').map(&:strip).inject(Set.new) { |set, broadcaster|
+      set.add Follow.new(broadcaster, subscriber)
+    }.to_a
   end
 
   private
@@ -24,31 +30,38 @@ class UserLine
   VALID_PATTERN = /.+ follows .+/i
 end
 
-VALID_LINES = ['Rob follows John', 'Rob follows John, Rich, Toby']
+VALID_LINES = ['Rob follows John', 'Rob follows John, Rich, Toby, John']
 INVALID_LINES = ['Rob', '', 'Rob > John', 'follows John', 'Rob follows']
 
 describe UserLine do
-  describe "#follows_by_user" do
-    it "returns all users with their follows" do
+  describe '#follows' do
+    it 'returns all users with their follower' do
       UserLine.new(VALID_LINES.first).
-        follows_by_user.must_equal({'Rob' => ['John']})
+        follows.must_equal([Follow.new('John', 'Rob')])
+
+      UserLine.new(VALID_LINES.last).
+        follows.must_equal([
+          Follow.new('John', 'Rob'),
+          Follow.new('Rich', 'Rob'),
+          Follow.new('Toby', 'Rob')
+        ])
     end
   end
 
-  describe "#valid?" do
-    describe "with valid line" do
+  describe '#valid?' do
+    describe 'with valid line' do
       VALID_LINES.each do |line|
         let (:user_line) { UserLine.new(line) }
-        it "must be true" do
+        it 'must be true' do
           user_line.valid?.must_equal true
         end
       end
     end
 
-    describe "with invalid line" do
+    describe 'with invalid line' do
       INVALID_LINES.each do |line|
         let (:user_line) { UserLine.new(line) }
-        it "must be false" do
+        it 'must be false' do
           user_line.valid?.must_equal false
         end
       end
